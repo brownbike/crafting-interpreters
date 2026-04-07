a4a4d1ec75f0d7e9dbc05fb820e0761669055cbe
Scott McKeon <scottmckeon@protonmail.com>
2026-04-07 09:12:12 -0700

    Add boilerplate lox class and types

---
+++ Lox.java
@@ -0,0 +1,69 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+// In Java, the `static` keyword is used to indicate that a variable or method belongs to the class
+// itself rather than to instances of the class. This means that static members are shared among
+// all instances, and you can access them without creating an object of the class.
+
+public class Lox {
+
+  static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  public static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+
+    // Indicate an error in teh exit code
+    if (hadError) System.exit(65);
+  }
+
+  public static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
+    }
+  }
+
+  public static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.error.println("[line " + line + "] Error " + where + ": " + message);
+    hadError = true;
+  }
+}
